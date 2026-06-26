bac0dbf1f0bd627dd7fc12f749e831759e76abbc
Doc1998 <docmp98@gmail.com>
2026-06-26 15:51:43 +0100

    UPDATES

---
+++ src/_data/client.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "name": "Revival Exterior Cleaning",
+    "email": "@gmail.com",
+    "phoneForTel": "+353832809535",
+    "phoneFormatted": "(083) 280 9535",
+    "address": {
+        "lineOne": "",
+        "lineTwo": "",
+        "city": "Carlow",
+        "state": "",
+        "zip": "",
+        "country": "IE",
+        "mapLink": ""
+    },
+    "socials": {
+        "facebook": "",
+        "instagram": "https://www.instagram.com",
+    },
+    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+    "domain": "https://www.revivalexteriorcleaning.ie"
+};
